48cace3cf41975165fe5d01139a8b76d14379497
benji3395 <benjiblank95@gmail.com>
2020-06-02 15:34:52 +0200

    Anmeldung Design weiter und Layout Veraenderung

--- Software/Vemini/Vemini/Vemini/Anmeldung.xaml.cs
+++ Software/Vemini/Vemini/Vemini/Anmeldung.xaml.cs
@@ -16,5 +16,38 @@ namespace Vemini
         {
             InitializeComponent();
         }
-	}
+
+        public string getEmail()
+        {
+            return email_entry.Text;
+        }
+
+        public string getPassword()
+        {
+            return password_entry.Text;
+        }
+
+        private void Button_OnClickedAnmelden(object sender, EventArgs e)
+        {
+            if (getEmail() == "" || getEmail() == null || getPassword() == "" || getPassword() == null)
+            {
+                DisplayAlert("Warnung","Emailadresse und/oder Passwort ungueltig", "ok");
+            }
+            else
+            {
+                //Uebermittlung an Datenbank
+            }
+        }
+
+        private void Button_OnClickedPwVerg(object sender, EventArgs e)
+        {
+            //Rücksetzen per Email Versendung?
+        }
+
+        private async void Button_OnClickedRegist(object sender, EventArgs e)
+        {
+            //Öffnen des Registrierungsformular
+            await Navigation.PushAsync(new Registrierung());
+        }
+    }
 }
