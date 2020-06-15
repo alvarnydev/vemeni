972121135809c3d7dc9a821899ed0993e16a11df
benji3395 <benjiblank95@gmail.com>
2020-06-15 17:52:26 +0200

    ErrandView Page rohbau + ErrandCreate Rohbau

---
+++ Software/Vemini/Vemini/Vemini/AppNavigation/ErrandView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vemini.AppNavigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ErrandView : ContentPage
+	{
+		public ErrandView ()
+		{
+			InitializeComponent ();
+        }
+
+        private async void MenuItem_OnClickedPlus(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreateErrand());
+        }
+    }
+}
