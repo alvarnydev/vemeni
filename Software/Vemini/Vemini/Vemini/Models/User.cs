dc12d984e85da39c05c8850f36d91d2a8f700e78
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-06-17 11:15:06 +0200

    Modified class User to gets datas from DB

--- Software/Vemini/Vemini/Vemini/Models/User.cs
+++ Software/Vemini/Vemini/Vemini/Models/User.cs
@@ -21,16 +21,16 @@ namespace Vemini
         public string Email { get; set; }
 
         [JsonProperty("verified_email")]
-        public bool VerifiedEmail { get; set; }
+        public bool VerifiedEmail { get; set; } // TODO DELETE
 
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        [JsonProperty("username")]
+        public string UserName { get; set; }
 
         [JsonProperty("given_name")]
-        public string GivenName { get; set; }
+        public string GivenName { get; set; } // TODO DELETE
 
         [JsonProperty("family_name")]
-        public string FamilyName { get; set; }
+        public string FamilyName { get; set; } // TODO DELETE
 
         [JsonProperty("link")]
         public string Link { get; set; }
@@ -39,6 +39,52 @@ namespace Vemini
         public string Picture { get; set; }
 
         [JsonProperty("gender")]
-        public string Gender { get; set; }
+        public string Gender { get; set; } // TODO DELETE
+
+
+
+
+        [JsonProperty("address_city")]
+        public string City { get; set; }
+
+        [JsonProperty("address_plz")]
+        public string PLZ { get; set; }
+
+        [JsonProperty("address_street")]
+        public string Street { get; set; }
+
+        [JsonProperty("lastvisit")]
+        public string LastVisit { get; set; }
+
+        [JsonProperty("created")]
+        public string  Created { get; set; }
+
+
+        public User() { }
+
+        public User(string id, string email, bool verifiedEmail, string name, string givenName, string familyName, string link, string picture, string gender, string city, string pLZ, string street, string lastVisit, string created
+            )
+        {
+            Id = id;
+            Email = email;
+            VerifiedEmail = verifiedEmail;
+            UserName = name;
+            GivenName = givenName;
+            FamilyName = familyName;
+            Link = link;
+            Picture = picture;
+            Gender = gender;
+            City = city;
+            PLZ = pLZ;
+            Street = street;
+            LastVisit = lastVisit;
+            Created = created;
+        }
+
+        public override string ToString()
+        {
+            return $"User: id = {Id},  username = {UserName}, email = {Email},\n" +
+                $"created = {Created}, lastvisit = {LastVisit}" ;
+        }
     }
 }
