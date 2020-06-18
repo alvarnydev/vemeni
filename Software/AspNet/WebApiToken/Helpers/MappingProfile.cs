48f699989c8d1bec62b518ca4c41b0b3ef8b1547
Yannik Sahl <s0569340@htw-berlin.de>
2020-06-18 15:49:46 +0200

    Added mapper for user - usermodel

---
+++ Software/AspNet/WebApiToken/Helpers/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebApiToken.Entities;
+using WebApiToken.Models;
+
+namespace WebApiToken.Helpers
+{
+    // This class maps objects, so that e.g. the UserModel (that is returned by the API) can be mapped to the User (that is the object stored in the database)
+    public class MappingProfile : Profile
+    {
+
+        // Constructor
+        public MappingProfile()
+        {
+
+            CreateMap<User, UserModel>();
+
+        }
+
+    }
+}
