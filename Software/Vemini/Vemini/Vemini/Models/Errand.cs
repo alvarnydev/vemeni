176c4af9cafd89883b43223728d7ea0ce9d273d7
benji3395 <benjiblank95@gmail.com>
2020-06-22 09:50:50 +0200

    erstellung Errand.cs und ErrandService.cs

---
+++ Software/Vemini/Vemini/Vemini/Models/Errand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Vemini.Models
+{
+    public class Errand
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("type")]
+        public bool Type { get; set; } 
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("location_lon")]
+        public string Location_lon { get; set; }
+
+        [JsonProperty("location_lat")]
+        public bool location_lat { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("status")]
+        public bool Status { get; set; }
+
+        [JsonProperty("accepted_by")]
+        public string Acceptedd_by { get; set; }
+    }
+}
---
+++ Software/Vemini/Vemini/Vemini/Services/ErrandService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Vemini.Models;
+
+
+namespace Vemini.Services
+
+{
+    public class ErrandService
+    {
+        public void AddErrand(Errand errand)
+        {
+            string url = "https://vergissmeinnicht.f2.htw-berlin.de/api/jobs";
+            var httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+            httpWebRequest.Method = "PUT";
+        }
+    }
+}
